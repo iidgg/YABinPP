232b61773475c2448b5b9e03a0cdcc8f17f77bab
id <ello@iduu.net>
2025-02-06 14:44:47 +0300

    feat: Add owned pastes table

---
+++ src/routes/dashboard/+page.server.ts
@@ -0,0 +1,62 @@
+import { redirect } from '@sveltejs/kit';
+import { getUserIdFromCookie } from '../../lib/server/auth';
+import prisma from '@db';
+import { type Actions } from '@sveltejs/kit';
+
+// maybe move that into an env var?
+const PAGE_SIZE = 10;
+
+export async function load({ cookies }) {
+    const userId = await getUserIdFromCookie(cookies);
+    if (!userId) redirect(303, '/login');
+
+    const pastes = await prisma.paste.findMany({
+        where: { ownerId: userId },
+        orderBy: { createdAt: 'desc' },
+        select: {
+            key: true,
+            createdAt: true,
+            expiresAt: true,
+            readCount: true,
+            language: true,
+            expiresCount: true,
+        },
+        take: PAGE_SIZE,
+    });
+
+    return { pastes, pageSize: PAGE_SIZE };
+}
+
+export const actions: Actions = {
+    fetch: async ({ cookies, request }) => {
+        const userId = await getUserIdFromCookie(cookies);
+        if (!userId) redirect(303, '/login');
+
+        const data = await request.formData();
+        const _before = data.get('before')?.toString();
+        const _after = data.get('after')?.toString();
+        const before = _before ? new Date(_before) : undefined;
+        const after = _after ? new Date(_after) : undefined;
+
+        if (!before && !after) return { pastes: [] };
+
+        const pastes = await prisma.paste.findMany({
+            where: {
+                ownerId: userId,
+                createdAt: { gt: after, lt: before },
+            },
+            select: {
+                key: true,
+                createdAt: true,
+                expiresAt: true,
+                readCount: true,
+                language: true,
+                expiresCount: true,
+            },
+            orderBy: { createdAt: 'desc' },
+            take: PAGE_SIZE,
+        });
+
+        return { pastes, pageSize: PAGE_SIZE };
+    },
+};
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+    import { enhance } from '$app/forms';
+    import Zeppelin from '@tabler/icons-svelte/icons/zeppelin';
+    import type { ActionData, PageData } from './$types';
+    import { IconArrowUpRight, IconArrowCapsule } from '@tabler/icons-svelte';
+
+    export let data: PageData;
+    export let form: ActionData;
+    let pastes = [...data.pastes];
+
+    $: if (form) pastes = [...pastes, ...form.pastes];
+
+    function formatDate(date: Date | null): string {
+        return date
+            ? date.toLocaleString('en-GB', {
+                  dateStyle: 'short',
+                  timeStyle: 'long',
+              })
+            : '';
+    }
+</script>
+
+<table
+    class="w-full text-sm text-left rtl:text-right border-2 border-gray-700 text-gray-400"
+>
+    <thead class="text-xs uppercase bg-gray-900 text-gray-400">
+        <tr>
+            <th scope="col" class="px-6 py-3">key</th>
+            <th scope="col" class="px-6 py-3">name</th>
+            <th scope="col" class="px-4 py-3 text-center">language</th>
+            <th scope="col" class="px-6 py-3 text-center">expires at</th>
+            <th scope="col" class="px-6 py-3 text-center">created at</th>
+            <th scope="col" class="px-2 py-3 text-center">reads</th>
+            <th></th>
+            <th></th>
+        </tr>
+    </thead>
+
+    <tbody>
+        {#each pastes as paste, i (paste.key)}
+            <tr class="bg-gray-800" class:bg-gray-700={i % 2 === 1}>
+                <th scope="row" class="py-4 px-6 font-medium text-gray-300">
+                    {paste.key}
+                </th>
+                <td class="py-4 px-6">-</td>
+                <td class="py-4 px-4 text-center">{paste.language}</td>
+                <td class="py-4 px-4 text-center">
+                    {formatDate(paste.expiresAt)}
+                </td>
+                <td class="py-4 px-4 text-center">
+                    {formatDate(paste.createdAt)}
+                </td>
+                <td class="py-4 text-center">{paste.readCount}</td>
+                <td class="py-4 px-3 text-center">
+                    <a
+                        href={`/${paste.key}/edit`}
+                        class="text-amber-500 hover:text-ambed-600"
+                        >edit<IconArrowUpRight class="inline" size="16" />
+                    </a>
+                </td>
+                <td class="py-4 px-3 text-center">
+                    <a
+                        href={`/${paste.key}`}
+                        class="text-cyan-400 hover:text-cyan-600"
+                        >read<IconArrowUpRight class="inline" size="16" />
+                    </a>
+                </td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
+
+<form
+    method="post"
+    action="?/fetch"
+    class="flex w-full justify-between"
+    use:enhance={(input) => {
+        const lastPaste = pastes[pastes.length - 1];
+
+        if (lastPaste) {
+            input.formData.set('before', lastPaste.createdAt.toString());
+        }
+    }}
+>
+    <p class="py-3 pl-4 text-gray-500 text-sm">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
+        perspiciatis tempora rerum?
+    </p>
+    <button class="px-4 bg-green-500 text-black hover:bg-green-600"
+        >load more <IconArrowCapsule class="inline" size="16" /></button
+    >
+</form>
