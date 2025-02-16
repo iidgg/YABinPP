975a1d4986688976178d2d9062ff220f835efd2a
id <ello@iduu.net>
2025-02-16 22:02:47 +0300

    feat: Implement TwoFA activation

---
+++ src/routes/api/@me/2fa/totp/activate/+server.ts
@@ -0,0 +1,49 @@
+import { json, type RequestHandler } from '@sveltejs/kit';
+import { getUserIdFromCookie } from '$lib/server/auth';
+import { TwoFA } from '$lib/constants/twoFAs';
+import { TOTP } from '$lib/server/2fa';
+import prisma from '@db';
+
+export const POST: RequestHandler = async ({ request, cookies }) => {
+    const { otp } = await request.json();
+    const userId = await getUserIdFromCookie(cookies);
+
+    if (!userId) {
+        return json(
+            { success: false, error: 'Unauthorized access' },
+            { status: 401 },
+        );
+    }
+
+    const user = await prisma.user.findFirst({ where: { id: userId } });
+
+    if (!user || !user.verified) {
+        return json(
+            { success: false, error: 'Account not verified' },
+            { status: 401 },
+        );
+    }
+
+    const twoFA = await prisma.twoFA.findUnique({
+        where: { userId_type: { userId, type: TwoFA.TOTP } },
+    });
+
+    if (!twoFA || twoFA.active) {
+        return json({ success: false }, { status: 404 });
+    }
+
+    if (TOTP.validate(twoFA.secret, otp)) {
+        await prisma.twoFA.update({
+            where: { userId_type: { userId, type: TwoFA.TOTP } },
+            data: { active: true },
+        });
+
+        return json({
+            success: true,
+        });
+    }
+
+    return json({
+        success: false,
+    });
+};
