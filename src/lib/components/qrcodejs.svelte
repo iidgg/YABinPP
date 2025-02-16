8075e1a5e8b9c4ab953c71abd91ff2b159fa0b00
id <ello@iduu.net>
2025-02-17 02:05:21 +0300

    fix: Update Qrcodejs when value changes

--- src/lib/components/qrcodejs.svelte
+++ src/lib/components/qrcodejs.svelte
@@ -6,24 +6,41 @@
     export let size;
 
     let qrcode;
+    let parent;
+    let loadedScript = false,
+        loadedQR = false;
+
+    $: {
+        if (parent && loadedScript) newQrcode();
+    }
+
+    const newQrcode = () => {
+        if (!loadedScript || !value) return;
+        qrcode = new QRCode('qrcode', {
+            text: value,
+            width: size,
+            height: size,
+            colorDark: '#000000',
+            colorLight: '#ffffff',
+            correctLevel: QRCode.CorrectLevel.H,
+        });
+        if (!loadedQR) loadedQR = true;
+    };
 
     onMount(() => {
+        if (loadedScript) return;
         let script = document.createElement('script');
         script.src =
             'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js';
         document.head.append(script);
 
         script.onload = function () {
-            qrcode = new QRCode('qrcode', {
-                text: value,
-                width: size,
-                height: size,
-                colorDark: '#000000',
-                colorLight: '#ffffff',
-                correctLevel: QRCode.CorrectLevel.H,
-            });
+            newQrcode();
+            loadedScript = true;
         };
     });
 </script>
 
-<div id="qrcode"></div>
+{#key value}
+    <div bind:this={parent} id="qrcode" />
+{/key}
