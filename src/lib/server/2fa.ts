19edfc2a8940e163c7988c6afd3bebb4098820db
id <ello@iduu.net>
2025-02-17 00:47:09 +0300

    fix: replace otplib with totp-generator

--- src/lib/server/2fa.ts
+++ src/lib/server/2fa.ts
@@ -1,22 +1,26 @@
-import { totp } from 'otplib';
+import { TOTP as totp } from 'totp-generator';
 import { randomBytes } from 'node:crypto';
 import base32 from 'hi-base32';
 import { TWOFA_ISSUER } from '../constants/twoFAs';
 
 export class TOTP {
+    static opts = {
+        algorithm: 'SHA-1',
+        digits: 6,
+        period: 30,
+    } as const;
+
     static validate(secret: string, otp: string) {
-        return totp.generate(secret) === otp;
+        return totp.generate(secret, this.opts).otp === otp;
     }
 
     static generateSecret() {
-        // 16 byte, 128-bit, 32 hex chars
-        return base32.encode(new Uint8Array(randomBytes(16)));
+        // 20 byte, 160-bit, 40 hex chars
+        return base32.encode(randomBytes(20));
     }
 
     static secretURI(username: string, secret: string) {
-        const opts = totp.allOptions();
-        const algorithm = opts.algorithm.toUpperCase();
-
-        return `otpauth://totp/${TWOFA_ISSUER}:${username}?algorithm=${algorithm}&digits=${opts.digits}&period=${opts.step}&issuer=${TWOFA_ISSUER}&secret=${secret}`;
+        const { algorithm, digits, period } = TOTP.opts;
+        return `otpauth://totp/${TWOFA_ISSUER}:${username}?algorithm=${algorithm.replaceAll('-', '')}&digits=${digits}&period=${period}&issuer=${TWOFA_ISSUER}&secret=${secret}`;
     }
 }
