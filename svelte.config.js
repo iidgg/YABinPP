44dba788f8469a8758fbe363f7e131b68c232fac
id <ello@iduu.net>
2025-02-25 15:03:27 +0300

    drop vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,14 @@
 import adapter from '@sveltejs/adapter-node';
-import adapterVercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-/** @type {import('@sveltejs/adapter-vercel').Config} */
-export const adapterVercelConfig = {
-    runtime: 'nodejs18.x',
-    regions: ['sin1'],
-    memory: 512,
-};
-
-const isVercel = process.env.VERCEL !== undefined; // Deploy to Vercel
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // Consult https://svelte.dev/docs/kit/integrations
     // for more information about preprocessors
     preprocess: vitePreprocess(),
 
     kit: {
-        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-        adapter: isVercel ? adapterVercel(adapterVercelConfig) : adapter(),
+        adapter: adapter(),
         alias: {
             '@db': './src/lib/server/prisma/prisma.ts',
         },
