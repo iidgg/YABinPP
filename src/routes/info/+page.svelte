7b3571a61bff8d9924f15ee63432dfab6af24599
id <ello@iduu.net>
2025-02-25 20:21:28 +0300

    update info

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import { goto } from '$app/navigation';
     import { onMount } from 'svelte';
-    import { INSTANCE_NAME } from '../../lib/publicEnv';
-    import { detectMac } from '../../lib/utils/util';
+    import { INSTANCE_NAME } from '$lib/publicEnv';
+    import { detectMac } from '$lib/utils/util';
 
     let cmdKey = 'Ctrl';
     onMount(() => {
@@ -47,98 +47,89 @@
     </div>
 
     <div class="p-1 md:px-4 md:py-2">
-        <div>
+        <div class="flex w-full justify-around">
             <h1 class="text-5xl text-gray-200 mb-6">
-                <b class="text-amber-500">Y</b>et
-                <b class="text-amber-500">A</b>nother Paste<b
-                    class="text-amber-500">bin</b
-                >
+                <span class="whitespace-nowrap">
+                    <b class="text-amber-500">Y</b>et
+                </span>
+                <span class="whitespace-nowrap">
+                    <b class="text-amber-500">A</b>nother
+                </span>
+                <span class="whitespace-nowrap">
+                    Paste<b class="text-amber-500">bin++</b>
+                </span>
             </h1>
-            <p class="my-4">
-                Why would I create another pastebin, when there are dozens on
-                the market already? Well, cause no pastebin I could find had ALL
-                of the following features:
-            </p>
-            <ul class="list-outside ml-4 list-disc">
-                <li>
-                    Modern and minimal UI (This site's design was inspired by <a
-                        class="underline underline-offset-2"
-                        href="https://github.com/wantguns/bin">bin</a
-                    >).
-                </li>
-                <li>
-                    <b>Optional</b> end-to-end encryption (we're using AES-256-GCM)
-                    with optional password protection (using PBKDF2-SHA512). The
-                    initialization vector is stored on the server, since it can be
-                    public and it results in a shorter URL while having the same
-                    end-to-end security.
-                </li>
-                <li>
-                    Syntax highlighting (using <a
-                        class="underline underline-offset-2"
-                        href="https://prismjs.com/">Prism</a
-                    >) that supports 297 languages.
-                </li>
-                <li>API support to create and get pastes from command line.</li>
-                <li>
-                    View raw pastes. Normally, encrypted pastebins do not have
-                    this. With this site, you can either get the Base64-encoded
-                    encrypted paste, or decrypt it on the server side (even with
-                    the password) and get the raw paste.
-                </li>
-                <li>Keyboard shortcuts!</li>
-                <li>
-                    And ofcourse, being fully <a
-                        class="underline underline-offset-2"
-                        href="https://github.com/Yureien/YABin">open-source</a
-                    > and easily self-hostable.
-                </li>
-                <li>
-                    <a
-                        class="underline underline-offset-2"
-                        href="https://github.com/Yureien/YABin/tree/main/cli"
-                        >CLI Client!</a
-                    >
-                </li>
-                <li>It can even be run in serverless environments!</li>
-            </ul>
-
-            <p class="mt-4">
-                The API documentation is available <a
-                    class="underline underline-offset-2"
-                    href="https://github.com/Yureien/YABin/blob/main/API.md"
-                    >here</a
-                >.
-            </p>
         </div>
 
-        <div class="mt-8">
-            <h1 class="text-4xl">Support the development</h1>
-            <p class="mt-2">
-                If you really like this project, I'd love it if you can give me
-                a <a
+        <p>
+            <!-- can write some description here -->
+        </p>
+
+        <br />
+
+        <h3 class="font-bold text-3xl">Features</h3>
+
+        <br />
+
+        <ul class="list-outside ml-8 list-disc">
+            <li>
+                Syntax highlighting (using <a
                     class="underline underline-offset-2"
-                    href="https://github.com/Yureien/YABin"
-                    aria-label="Star Yureien/YABin on GitHub">Star</a
-                > on GitHub! That provides me with motivation, seeing people use
-                stuff I make.
+                    href="https://prismjs.com/">Prism</a
+                >) that supports 297 languages.
+            </li>
+            <li>
+                Optional end-to-end encryption with a randomly generated key or
+                a key derived from a password.
+            </li>
+            <li>Optional Server-Side encryption/decryption</li>
+        </ul>
+
+        <br />
+
+        <h6 class="font-bold text-lg">
+            - API (<a
+                class="text-blue-300 underline underline-offset-2 hover:text-blue-400"
+                href="https://github.com/Yureien/YABin/blob/main/API.md">docs</a
+            >)
+        </h6>
+        <ul class="list-outside ml-8 list-disc">
+            <li>Scoped tokens for API authentication</li>
+            <li>Optional Server-Side encryption/decryption</li>
+        </ul>
+
+        <br />
+    </div>
+
+    <div class="flex w-full justify-around">
+        <div
+            class="flex flex-col lg:flex-row justify-between items-center mx-2 w-full xl:w-3/4 text-gray-300"
+        >
+            <p class="whitespace-nowrap">
+                Site's source
+                <a
+                    class="text-blue-300 underline underline-offset-2 hover:text-blue-400"
+                    href="https://github.com/iidgg/YABinPP"
+                    >iidgg/YABinPP
+                </a>
             </p>
-            <p>
-                If you do not mind donating, then please <a
-                    href="https://ko-fi.com/A0A21C34E"
-                    target="_blank"
-                    ><img
-                        class="border-0 inline h-8"
-                        src="https://storage.ko-fi.com/cdn/kofi3.png?v=3"
-                        alt="Buy Me a Coffee at ko-fi.com"
-                    /></a
-                >
-                or
-                <iframe
-                    src="https://github.com/sponsors/Yureien/button"
-                    title="Sponsor Yureien"
-                    class="border-0 rounded h-8 inline w-28"
-                /> me on GitHub. Although, a star on GitHub is more than enough!
+
+            <p class="whitespace-nowrap">
+                Source forked from
+                <a
+                    class="text-blue-300 underline underline-offset-2 hover:text-blue-400"
+                    href="https://github.com/Yureien/YABin"
+                    >Yureien/YABin
+                </a>
+            </p>
+
+            <p class="whitespace-nowrap">
+                Design inspired by
+                <a
+                    class="text-blue-300 underline underline-offset-2 hover:text-blue-400"
+                    href="https://github.com/wantguns/bin"
+                    >wantguns/bin
+                </a>
             </p>
         </div>
     </div>
